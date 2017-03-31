a7910dc96c989febdb06d732a39fc00796a284ff
Thura Aung <thura.ucsy@gmail.com>
2017-03-31 10:21:55 +0900

    Update main.js

--- webrtc/main.js
+++ webrtc/main.js
@@ -22,15 +22,23 @@ function getSources_() {
   }
   navigator.mediaDevices.enumerateDevices().then(function(devices) {
     for (var i = 0; i < devices.length; i++) {
-	    setTimeout(function(){
-				    console.log("devices[i].label", devices[i].label)
-				      if (devices[i].kind === 'videoinput') {
-					deviceList[i] = devices[i];
-					console.info("requesting devices");
-					requestVideo_(deviceList[i].deviceId);
-				      }
-		    }, 2000);
+	    
+		console.log("devices[i].label", devices[i].label)
+	      if (devices[i].kind === 'videoinput') {
+		deviceList[i] = devices[i];
+		console.info("requesting devices");
+	      }
+		 
     }
+	  
+	  setTimeout(function(){  
+	  	requestVideo_(deviceList[0].deviceId);
+	  }, 2000);
+	  
+	  	  
+	  setTimeout(function(){  
+	  	requestVideo_(deviceList[1].deviceId);
+	  }, 2000);
   });
 }
 
