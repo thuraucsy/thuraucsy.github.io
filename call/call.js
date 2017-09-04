99151c1fa700fea734b4abb266e0933df0ce67be
Thura Aung <thura.ucsy@gmail.com>
2017-09-04 14:31:49 +0630

    Add files via upload

---
+++ call/call.js
@@ -0,0 +1,32 @@
+var session;
+
+var endButton = document.getElementById('endCall');
+endButton.addEventListener('click', function() {
+	session.bye();
+	alert('Call Ended');
+}, false);
+
+var startButton = document.getElementById('startCall');
+startButton.addEventListener('click', function(){
+	session = userAgent.invite('sip:2023@203.181.36.238', options);
+	alert('Call started');
+}, false);
+
+var userAgent = new SIP.UA({
+	uri: '2000@203.181.36.238',
+	wsServers: ['ws://203.181.36.238'],
+	password: 'PASS'
+});
+
+var options = {
+	media: {
+		constraints: {
+			audio: true,
+			video: true
+		},
+		render: {
+			remote: document.getElementById('remoteVideo'),
+			local: document.getElementById('localVideo')
+		}
+	}
+};
